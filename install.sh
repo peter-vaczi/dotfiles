d325c5a24c05d2029805379f63b9688d2f817ae4
Peter Vaczi <peter.vaczi@nokia.com>
2022-01-22 16:22:08 +0100

    do a hard cleanup

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-if [ ! -d ~/.dotfiles ]; then
-  git clone git://github.com/peter-vaczi/config_stuff.git ~/.dotfiles
-fi
-
-ln -sf ~/.dotfiles/freshrc ~/.freshrc
-bash -c "$(curl -sL get.freshshell.com)"
